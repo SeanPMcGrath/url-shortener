e729aec0f2910e2393a7c0555a64f7c5e3839689
Sean McGrath <spmcgrath9@gmail.com>
2019-07-07 17:07:06 -0700

    Submit button generally working. Need to get requests to register server-side

--- client/js/index.js
+++ client/js/index.js
@@ -5,6 +5,33 @@
 // }
 // initialSetting();
 
-window.urlSubmit = function() {
-  alert("Button was clicked");
-};
+function urlSubmit() {
+  let textValue = $("#original_url").val();
+  $("#originalLink").text(textValue);
+
+  const urlObject = JSON.stringify({
+    original_url: $("#original_url").val()
+  });
+
+  $.get("http://localhost:5000/api/shorturls", function(data) {
+    $("#newLink").append("Data: " + data);
+  });
+
+  // $.ajax({
+  //   url: "http://localhost:5000/api/shorturl/new",
+  //   type: "POST",
+  //   data: urlObject,
+  //   dataType: "JSON",
+  //   beforeSend: () => {
+  //     $("#newLink").append("<br>beforeSend<br>");
+  //   },
+  //   error: () => {
+  //     $("#newLink").append("<br>error<br>");
+  //   },
+  //   success: () => {
+  //     $("#newLink").append("<br>success<br>");
+  //   }
+  // });
+
+  //$("#newLink").append(urlObject);
+}
